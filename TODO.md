779c58b27a7b592c77d9bf637793fbb31ca12a4d
Andrew Stuart <andrew.stuart2@gmail.com>
2017-05-15 15:50:19 -0700

    Update docs

--- TODO.md
+++ TODO.md
@@ -1,4 +1,3 @@
 # TODO
-
 - disk caching client
 - CSRF
--- cli.go
+++ cli.go
@@ -2,14 +2,21 @@ package clyde
 
 import "net/http"
 
+// RoundTripFunc creates an http.RoundTripper from a function that returns a
+// response/error
 type RoundTripFunc func(*http.Request) (*http.Response, error)
 
+// RoundTrip implements http.RoundTripper
 func (r RoundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
 	return r(req)
 }
 
+// A HeaderRoundTripper is a map of header keys to header values. The
+// HeaderRoundTripper is not safe for concurrent modification and use, as it is
+// purely a raw map.
 type HeaderRoundTripper map[string]string
 
+// RoundTrip implements http.RoundTripper
 func (h HeaderRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
 	for k, v := range h {
 		req.Header.Set(k, v)
